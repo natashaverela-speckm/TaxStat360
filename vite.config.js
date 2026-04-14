b2add350fcff3a020a14d66352e9bb0df01fd534
TaxStat360 <natasha.verela@gmail.com>
2026-04-13 20:36:45 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: { target: 'es2020' }
+  esbuild: { target: 'es2020' },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-v2.js`,
+        chunkFileNames: `assets/[name]-[hash]-v2.js`,
+      }
+    }
+  }
 })
