37007f4f742bb94418ac227a1f7ca6505463bf03
TaxStat360 <natasha.verela@gmail.com>
2026-04-15 22:51:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// v3 - force new bundle filename
+// v4 - force new bundle filename
 export default defineConfig({
   plugins: [react()],
   esbuild: { target: 'es2015' },
-  define: { __BUILD__: '"v3"' },
+  define: { __BUILD__: '"v4"' },
   build: {
     outDir: 'dist',
     rollupOptions: {
